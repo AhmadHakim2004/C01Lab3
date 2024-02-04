0e43abdb9452afb3f252666f8be2173e84f7ac0b
AhmadHakim2004 <hakimahmad2004@gmail.com>
2024-02-04 18:46:07 -0500

    Integrate deleteAllNotes endpoint

--- quirknotes/backend/server.js
+++ quirknotes/backend/server.js
@@ -144,3 +144,16 @@ app.patch("/patchNote/:noteId", express.json(), async (req, res) => {
     res.status(500).json({error: error.message})
   }
 })
+
+// Delete all notes
+app.delete("/deleteAllNotes", express.json(), async (req, res) => {
+	try {
+	  // Find note with given ID
+	  const collection = db.collection(COLLECTIONS.notes);
+	  const result = await collection.deleteMany({});
+  
+	  res.json({ response: `${result.deletedCount} deleted.` });
+	} catch (error) {
+	  res.status(500).json({ error: error.message });
+	}
+  })
